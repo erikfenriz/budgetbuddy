c8e7bb3d38dc1a2690a0792b335fd33bb008d14f
Erik Rybalkin <erikfenriz@gmail.com>
2018-06-25 19:08:23 +0300

    get value

--- app.js
+++ app.js
@@ -4,20 +4,47 @@ var budgetController = (function () {
 
 var UIController = (function () {
 
+    DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+
+    };
+
+
 })();
 
 var controller = (function (budgetCtrl, UICtrl) {
 
+    var DOM = UIController.getDOMstrings();
+
     var ctrlAddItem = function () {
+        var input = UICtrl.getInput();
+        console.log(input);
+
     };
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function (event) {
-        if(event.keyCode === 13 || event.which === 13){
+        if (event.keyCode === 13 || event.which === 13) {
             ctrlAddItem();
         }
     })
 
 
-})(budgetController,UIController);
+})(budgetController, UIController);
